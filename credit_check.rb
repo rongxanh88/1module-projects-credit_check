f124e36250ce4893ff1ef33df76ddc2b4e8ae977
rongxanh88 <rongxanh88@gmail.com>
2017-03-15 17:23:06 -0600

    Added double array and validate methods

--- credit_check.rb
+++ credit_check.rb
@@ -12,9 +12,17 @@
 
 class CreditCheck
   def check(card_number)
-    number array = clean_data(card_number)
+    num_array = clean_data(card_number)
 
+    if num_array.length != 16
+      puts "Number is invalid"
+      return
+    end
     
+    num_array = num_array.reverse #reverse so that we can start from the 'right'
+    doubled_array = double_array(num_array)
+    validity = validate(doubled_array)
+    return validity
   end
   
   def clean_data(number)
@@ -24,6 +32,35 @@ class CreditCheck
       return number.chars.map {|num| num.to_i}
     else
       puts "Number is invalid."
+    end
+  end
+
+  def double_array(array)
+    doubled_array = []
+    array.each_with_index {|num, index|
+      if index % 2 == 0 #double digits
+        num = num * 2
+        if num > 9
+          remainder = num % 10
+          doubled_array << (1 + remainder)
+        else
+          doubled_array << num
+        end
+      else
+        doubled_array << num
+      end
+    }
+    return doubled_array
+  end
+
+  def validate(num_array)
+    #sum array and check to equal 70
+    sum = num_array.reduce(:+)
+  
+    if sum == 70
+      return true
+    else
+      return false
     end
   end
   
