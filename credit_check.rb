210a1eb87b83896a47d76c7577fb4e2dc766cccb
rongxanh88 <rongxanh88@gmail.com>
2017-03-16 10:30:28 -0600

    Added function to check the AmEx numbers

--- credit_check.rb
+++ credit_check.rb
@@ -1,26 +1,17 @@
-#card_number = "4929735477250543"
-#Valid: 5541808923795240, 4024007136512380, 6011797668867828
-#Invalid: 5541801923795240, 4024007106512380, 6011797668868728
-
-#valid = false
-
-# Your Luhn Algorithm Here
-
-# Output
-## If it is valid, print "The number is valid!"
-## If it is invalid, print "The number is invalid!"
-
 class CreditCheck
   def check(card_number)
-    num_array = clean_data(card_number)
-    if num_array.length != 16
+    numbers = clean_data(card_number)
+    if numbers.length == 15
+      #need to make a check digit
+      numbers = add_check_digit(numbers)
+    elsif numbers.length != 16
       puts "Number is invalid"
       return
     end
     
-    num_array = num_array.reverse #reverse so that we can start from the 'right'
-    doubled_array = double_array(num_array)
-    return validate(doubled_array)
+    numbers = numbers.reverse #reverse so that we can start from the 'right'
+    doubled_nums = double_array(numbers)
+    return validate(doubled_nums)
   end
   
   def clean_data(number)
@@ -34,40 +25,60 @@ class CreditCheck
   end
 
   def double_array(array)
-    doubled_array = []
+    doubled_nums = []
     array.each_with_index {|num, index|
       if index % 2 == 1 #double digits
         num = num * 2
         if num > 9
           remainder = num % 10
-          doubled_array << (1 + remainder)
+          doubled_nums << (1 + remainder)
         else
-          doubled_array << num
+          doubled_nums << num
         end
       else
-        doubled_array << num
+        doubled_nums << num
       end
     }
-    return doubled_array
+    return doubled_nums
   end
 
-  def validate(num_array)
+  def validate(numbers)
     #sum array and check to equal to modulo 10 = 0
-    sum = num_array.reduce(:+)
+    sum = numbers.reduce(:+)
     if sum % 10 == 0
+      puts "The number is valid!"
       return true
     else
+      puts "The number is invalid!"
       return false
     end
+  end
+  
+  def add_check_digit(numbers)
+    numbers = numbers * 10 #adds zero at end to fix length at 16 digits
+    doubled_numbers = double_array(numbers)
+    sum = doubled_numbers.reduce(:+)
+    if sum % 10 == 0
+      return numbers
+    else
+      check_digit = sum % 10
+      numbers.pop()
+      numbers.push(check_digit)
+    end
+    return numbers
   end
   
 end
 
 checker = CreditCheck.new
-puts checker.check(4929735477250543)
-puts checker.check(6011797668867828) #valid
-puts checker.check(4024007136512380) #valid
-puts checker.check(5541808923795240) #valid
-puts checker.check(6011797668868728) #invalid
-puts checker.check(4024007106512380) #invalid
-puts checker.check(5541801923795240) #invalid
+checker.check(4929735477250543)
+checker.check(6011797668867828) #valid
+checker.check(4024007136512380) #valid
+checker.check(5541808923795240) #valid
+checker.check(6011797668868728) #invalid
+checker.check(4024007106512380) #invalid
+checker.check(5541801923795240) #invalid
+
+puts "Testing American Express numbers"
+checker.check(342804633855673) #valid
+checker.check(342801633855673) #invalid
